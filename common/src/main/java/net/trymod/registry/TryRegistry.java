d8813f91290761f0ee96f62fd42bc0483b5fd953
gadik <katak@gmail.com>
2024-01-24 05:12:22 +0800

    add registries

--- common/src/main/java/net/trymod/registry/TryRegistry.java
+++ common/src/main/java/net/trymod/registry/TryRegistry.java
@@ -2,9 +2,11 @@ package net.trymod.registry;
 
 import com.google.common.base.Suppliers;
 import dev.architectury.registry.CreativeTabRegistry;
+import dev.architectury.registry.client.rendering.RenderTypeRegistry;
 import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrarManager;
 import dev.architectury.registry.registries.RegistrySupplier;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
@@ -13,7 +15,6 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.material.Fluid;
 import net.trymod.fluid.OrangeJuiceFluid;
-import net.trymod.fluid.TryFluid;
 import net.trymod.item.OrangeJuiceCupItem;
 
 import java.util.function.Supplier;
@@ -23,15 +24,13 @@ import static net.trymod.TryMod.MOD_ID;
 public class TryRegistry {
     public static final Supplier<RegistrarManager> REGISTRIES = Suppliers.memoize(() -> RegistrarManager.get(MOD_ID));
 
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(MOD_ID, Registries.ITEM);
-    public static final RegistrySupplier<Item> ORANGEJUICE_CUP = ITEMS.register("orangejuice_cup", () -> new OrangeJuiceCupItem(false));
-    public static final RegistrySupplier<Item> ORANGEJUICE_CUP_EMPTY = ITEMS.register("orangejuice_cup_empty", () -> new OrangeJuiceCupItem(true));
-
     public static final DeferredRegister<Fluid> FLUIDS = DeferredRegister.create(MOD_ID, Registries.FLUID);
 
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(MOD_ID, Registries.BLOCK);
 
-    public static final OrangeJuiceFluid ORANGEJUICE_FLUID = new OrangeJuiceFluid().init();
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(MOD_ID, Registries.ITEM);
+    public static final RegistrySupplier<Item> ORANGEJUICE_CUP = ITEMS.register("orangejuice_cup", () -> new OrangeJuiceCupItem(false));
+    public static final RegistrySupplier<Item> ORANGEJUICE_CUP_EMPTY = ITEMS.register("orangejuice_cup_empty", () -> new OrangeJuiceCupItem(true));
 
     public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(MOD_ID, Registries.CREATIVE_MODE_TAB);
     public static final RegistrySupplier<CreativeModeTab> TRY_TAB = TABS.register(
@@ -41,4 +40,12 @@ public class TryRegistry {
                     () -> new ItemStack(ORANGEJUICE_CUP)
             )
     );
+    public static final OrangeJuiceFluid ORANGEJUICE_FLUID = new OrangeJuiceFluid().init();
+
+    @SafeVarargs
+    public static <T extends Fluid> void registerFluids(RenderType renderType, RegistrySupplier<T>... fluids) {
+        for (RegistrySupplier<T> fluidRegistry : fluids) {
+            fluidRegistry.listen( (fluid) -> RenderTypeRegistry.register(renderType, fluid));
+        }
+    }
 }
