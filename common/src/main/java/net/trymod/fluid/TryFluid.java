f5ab4619d559ac37ce8ce63b4882b1a8f51ce8bf
gadik <katak@gmail.com>
2024-01-24 05:13:33 +0800

    add constructor for custom LiquidBlock and add water overlay as default overlay

--- common/src/main/java/net/trymod/fluid/TryFluid.java
+++ common/src/main/java/net/trymod/fluid/TryFluid.java
@@ -5,26 +5,19 @@ import dev.architectury.core.fluid.ArchitecturyFlowingFluid;
 import dev.architectury.core.fluid.SimpleArchitecturyFluidAttributes;
 import dev.architectury.core.item.ArchitecturyBucketItem;
 import dev.architectury.registry.registries.RegistrySupplier;
-import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.data.tags.FluidTagsProvider;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.FluidTags;
-import net.minecraft.tags.ItemTags;
-import net.minecraft.tags.TagBuilder;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.LiquidBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.FlowingFluid;
-import net.minecraft.world.level.material.Fluid;
-import net.minecraft.world.level.material.Fluids;
+
+import java.util.function.Supplier;
 
 import static net.trymod.registry.TryRegistry.*;
 
-public class TryFluid {
+public class TryFluid { // this is probably stupid
     public final String name;
     public final RegistrySupplier<FlowingFluid> sourceRegistry;
     public final RegistrySupplier<FlowingFluid> flowingRegistry;
@@ -36,10 +29,23 @@ public class TryFluid {
         sourceRegistry = FLUIDS.register(name, () -> new ArchitecturyFlowingFluid.Source(attributes));
         flowingRegistry = FLUIDS.register(name + "_flowing", () -> new ArchitecturyFlowingFluid.Flowing(attributes));
         blockRegistry = BLOCKS.register(name + "_block", () -> new ArchitecturyLiquidBlock(sourceRegistry, BlockBehaviour.Properties.ofFullCopy(Blocks.WATER)));
-        bucketRegistry = ITEMS.register(name + "_bucket", () -> new ArchitecturyBucketItem(sourceRegistry, new Item.Properties().arch$tab(TRY_TAB)));
+        bucketRegistry = ITEMS.register(name + "_bucket", () -> new ArchitecturyBucketItem(sourceRegistry, new Item.Properties().craftRemainder(Items.BUCKET).arch$tab(TRY_TAB)));
         attributes.blockSupplier(() -> blockRegistry)
                 .bucketItemSupplier(() -> bucketRegistry)
                 .sourceTexture(new ResourceLocation("block/water_still"))
-                .flowingTexture(new ResourceLocation("block/water_flow"));
+                .flowingTexture(new ResourceLocation("block/water_flow"))
+                .overlayTexture(new ResourceLocation("block/water_overlay"));
+    }
+    public TryFluid(String name, SimpleArchitecturyFluidAttributes attributes, Supplier<ArchitecturyLiquidBlock> block) {
+        this.name = name;
+        sourceRegistry = FLUIDS.register(name, () -> new ArchitecturyFlowingFluid.Source(attributes));
+        flowingRegistry = FLUIDS.register(name + "_flowing", () -> new ArchitecturyFlowingFluid.Flowing(attributes));
+        blockRegistry = BLOCKS.register(name + "_block", block);
+        bucketRegistry = ITEMS.register(name + "_bucket", () -> new ArchitecturyBucketItem(sourceRegistry, new Item.Properties().craftRemainder(Items.BUCKET).arch$tab(TRY_TAB)));
+        attributes.blockSupplier(() -> blockRegistry)
+                .bucketItemSupplier(() -> bucketRegistry)
+                .sourceTexture(new ResourceLocation("block/water_still"))
+                .flowingTexture(new ResourceLocation("block/water_flow"))
+                .overlayTexture(new ResourceLocation("block/water_overlay"));
     }
 }
