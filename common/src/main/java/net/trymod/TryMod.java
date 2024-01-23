ac85f7b9bc0d8337546b3b7d0ebb552c29fc2eb6
gadik <katak@gmail.com>
2024-01-24 05:12:27 +0800

    add logger

--- common/src/main/java/net/trymod/TryMod.java
+++ common/src/main/java/net/trymod/TryMod.java
@@ -1,9 +1,13 @@
 package net.trymod;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static net.trymod.registry.TryRegistry.*;
 
 public class TryMod {
     public static final String MOD_ID = "trymod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     
     public static void init() {
         FLUIDS.register();
